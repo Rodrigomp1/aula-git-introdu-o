81d5232d80d24cb968cb18401c3a2a0065f05b75
Rodrigo <rodrigo.possel@rede.ulbra.br>
2026-04-29 21:21:33 -0300

    Adiciona arquivo de perfil

---
+++ perfil.py
@@ -0,0 +1,7 @@
+nome = "Rodrigo"
+curso = "Análise e Desenvolvimento de Sistemas"
+area_interesse = "Ensino, desenvolvimento, IA"
+
+print("Olá, meu nome é", nome)
+print("Curso: ", curso)
+print("Área de interesse: ", area-interesse)
